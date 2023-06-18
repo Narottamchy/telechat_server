2cf334d748c0a4ad203642e0bd1c7d4bd5056d24
Narottam Choudhary <narottamchy5134@gmail.com>
2023-06-18 16:49:13 +0530

    socket_handel

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 
 app.use('/', route);
 
@@ -26,7 +33,8 @@ const server = app.listen(PORT, () => {
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_SERVER,
+    origin: `${process.env.CLIENT_SERVER}`,
+    methods: ["GET", "POST"]
   },
 });
 
