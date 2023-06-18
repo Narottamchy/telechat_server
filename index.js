00f537d3752b7ef5f2b88ea9183b800863849d0c
Narottam Choudhary <narottamchy5134@gmail.com>
2023-06-18 15:49:18 +0530

    workings_Server

--- index.js
+++ index.js
@@ -5,71 +5,70 @@ import cors from 'cors';
 import dotenv from "dotenv";
 import bodyParser from 'body-parser';
 import { Server } from 'socket.io';
+
 dotenv.config();
 
 const PORT = process.env.HOST_PORT || 8000;
 
-
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/',route);
+app.use('/', route);
 
 Connection();
 
-const io = new Server(3000, {
-    cors: {
-        origin: `${process.env.CLIENT_SERVER}`,
-    }, 
-})
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_SERVER,
+  },
+});
 
 let users = [];
 
 const addUser = (userData, socketId) => {
-    !users.some(user => user.uid === userData.uid) && users.push({ ...userData, socketId });
-}
+  !users.some(user => user.uid === userData.uid) && users.push({ ...userData, socketId });
+};
 
 const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId);
-}
+  users = users.filter(user => user.socketId !== socketId);
+};
 
 const getUser = (userId) => {
-    return users.find(user => user.uid === userId);
-}
+  return users.find(user => user.uid === userId);
+};
 
-io.on('connection',  (socket) => {
-    // console.log('user connected')
+io.on('connection', (socket) => {
+  // console.log('user connected');
 
-    //connect
-    socket.on("addUsers", userData => {
-        addUser(userData, socket.id);
-        io.emit("getUsers", users);
-    })
+  // Connect
+  socket.on("addUsers", userData => {
+    addUser(userData, socket.id);
+    io.emit("getUsers", users);
+  });
 
-    //send message
-    socket.on('sendMessage', (data) => {
-        const user = getUser(data.receiverId);
-        io.to(user.socketId).emit('getMessage', data)
-    })
+  // Send message
+  socket.on('sendMessage', (data) => {
+    const user = getUser(data.receiverId);
+    io.to(user.socketId).emit('getMessage', data);
+  });
 
-    // Handle typing event
-    socket.on('typing', (data) => {
-        // Broadcast the typing event to all connected clients except the sender
-        socket.broadcast.emit('typing', data);
-      });
-    
+  // Handle typing event
+  socket.on('typing', (data) => {
+    // Broadcast the typing event to all connected clients except the sender
+    socket.broadcast.emit('typing', data);
+  });
 
-    //disconnect
-    socket.on('disconnect', () => {
-        // console.log('user disconnected');
-        removeUser(socket.id);
-        io.emit('getUsers', users);
-    })
-})
-
-
-app.listen(PORT)
+  // Disconnect
+  socket.on('disconnect', () => {
+    // console.log('user disconnected');
+    removeUser(socket.id);
+    io.emit('getUsers', users);
+  });
+});
